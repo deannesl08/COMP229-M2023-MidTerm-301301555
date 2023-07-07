176f02a8ce85ae2f47f9fb9ce98846f41f62ca86
Deanne Laylay <deannelaylay@gmail.com>
2023-07-07 13:47:41 -0400

    Midterm Exam

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,21 @@
+//module.exports = {
+
+  //url: "mongodb://localhost:27017/Blessing_db"
+
+ 
+
+  //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+   //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+
+//url:"mongodb+srv://Blessing:KF4CzZpj8tlYGtap@cluster0.29680fe.mongodb.net/SportStore?retryWrites=true&w=majority"
+//"URI":"mongodb+srv://laylaydeanne:laylaydeanne@clusterdress.meiqu9w.mongodb.net/DressStore"
+
+
+//};
+
+module.exports = {
+
+  url:"mongodb+srv://laylaydeanne:laylaydeanne@clusterdress.meiqu9w.mongodb.net/DressStore"
+
+}
